24e230a916fc20a61ed1b76657b0452f2a75f03b
ylesandale <75402045+ylesandale@users.noreply.github.com>
2021-04-04 12:48:47 +0300

    11

--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -21,7 +21,7 @@ const favorites: React.FC = () => {
       localStorage.getItem("favoriteVideos")
     );
     dispatch(setStartFavoriteVideo(localFavoriteVideos));
-  }, []);
+  }, [dispatch]);
 
   React.useEffect(() => {
     localStorage.setItem("favoriteVideos", JSON.stringify(favoriteVideos));
@@ -47,7 +47,7 @@ const favorites: React.FC = () => {
       <div className="main">
         <div className="main__content">
           <div className="content">
-            {favoriteVideos && favoriteVideos.length > 0 ? (
+            {favoriteVideos.length > 0 ? (
               favoriteVideos.map((video: Video) => (
                 <div key={video.id} className="content__item">
                   <a target="_blank" href={video.url} rel="noreferrer">
