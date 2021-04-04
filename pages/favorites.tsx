af29be8396b2539843451e9142e1dfce5e826deb
ylesandale <75402045+ylesandale@users.noreply.github.com>
2021-04-04 12:17:03 +0300

    Update favorites.tsx

--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -47,7 +47,7 @@ const favorites: React.FC = () => {
       <div className="main">
         <div className="main__content">
           <div className="content">
-            {favoriteVideos.length ? (
+            {favoriteVideos && favoriteVideos.length ? (
               favoriteVideos.map((video: Video) => (
                 <div key={video.id} className="content__item">
                   <a target="_blank" href={video.url} rel="noreferrer">
